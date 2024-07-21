3f7c500d66bbdd169934519cfe56bfeb32846f6a
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2024-07-20 20:50:55 -0700

    built out hero video for the homepage

--- src/Components/Page1/Page1Content.jsx
+++ src/Components/Page1/Page1Content.jsx
@@ -8,11 +8,25 @@ const Page1Content = () => {
       <div className="xlscreens">
         <div className="xlsinner">
           <div className="page1Main">
-          <div className="video-container">
-      <video className="full-screen-video" autoPlay loop muted>
-        <source src={Vid} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+          <div className="App">
+      <div className="video-background">
+        <video autoPlay loop muted>
+          <source src={Vid} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="content">
+        <p style={{
+          fontSize: "50px"
+        }}>Brian Williams</p>
+        <p style={{
+          fontSize: "80px"
+        }}>Full Stack Web Developer</p>
+        <div className="buttons">
+          <button>Button 1</button>
+          <button>Button 2</button>
+        </div>
+      </div>
     </div>
           </div>
         </div>
